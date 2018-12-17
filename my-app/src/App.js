9b741eee6bdf0a68f4aeb3a3ebd1114b3bc03ff9
paula.garcia <paula.garcia@intelygenz.com>
2018-12-17 15:51:08 +0100

    new Component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
+  
+  state = {
+    persons: [
+      { name: 'Mary', age: 48},
+      { name: 'Claudia', age: 28},
+      { name: 'Steph', age: 66}
+    ],
+    Hobbies: 'None'  
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState( {
+      persons: [
+        { name: newName, age: 48},
+        { name: 'Claudia', age: 28},
+        { name: 'Steph', age: 1}
+      ]
+    } )
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Leticia', age: 48},
+        { name: event.target.value, age: 28},
+        { name: 'Steph', age: 89}
+      ]
+    } )
+  }
+
+  render () {
+
+    const btnStyle = {
+      backgroundColor: 'gray',
+      color: 'white',
+      boxShadow: '2px 2px 4px gray',
+      padding: '5px 10px',
+      borderRadius: '10px',
+      outline: 'none',
+      cursor: 'pointer'
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <h1>Hi there!!!</h1>
+        <p>This is really working</p>
+        <button style={btnStyle} onClick={ () => this.switchNameHandler('Clotilde')}>Switch name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.Hobbies}
+          click={this.switchNameHandler.bind(this, 'Max')}
+          changed={this.nameChangeHandler}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
-    );
+    )
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi there!!!'))
   }
 }
 
-export default App;
+export default App
